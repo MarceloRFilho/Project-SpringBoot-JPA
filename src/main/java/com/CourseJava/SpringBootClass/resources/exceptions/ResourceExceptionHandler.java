207c3e78e4e3061b70408788156d16fb8b1a17a8
Marcelo R Filho <marcelo.1496@gmail.com>
2023-08-16 14:02:58 -0400

    Delete Exception

--- src/main/java/com/CourseJava/SpringBootClass/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/CourseJava/SpringBootClass/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.CourseJava.SpringBootClass.services.exceptions.DatabaseException;
 import com.CourseJava.SpringBootClass.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,7 +21,14 @@ public class ResourceExceptionHandler {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
-		
 	}
 
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
 }
